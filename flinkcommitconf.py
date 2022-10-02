47f7de6fbe1340f5822a777a3929a557b1cde3e3
735140144 <98571106+735140144@users.noreply.github.com>
2022-10-02 18:14:42 +0800

    Add files via upload

---
+++ flinkcommitconf.py
@@ -0,0 +1,24 @@
+"""
+@function:
+@parameter:
+@attention:
+"""
+
+# 数据库配置
+CONF_PORT = 3306
+CONF_HOST = ""
+CONF_USER = ""
+CONF_PASSWD = ""
+CONF_DB = ""
+
+# 接口配置
+POST_URL = ""
+
+# hdfs目录配置
+USER = ''
+NAMESPACE = ""
+hdfs_url = ""
+hdfs_path = ""
+
+# 本地目录配置
+LOCAL_PATH = ""
